8cb7e08915cabe188c3c54bcb782b99c62602036
tuankqhe130156 <tuankqhe130156@fpt.edu.vn>
2022-02-27 18:03:40 +0700

    update

---
+++ function.js
@@ -0,0 +1,33 @@
+// Code By Webdevtrick ( https://webdevtrick.com )
+var $header_top = $('.header-top');
+var $nav = $('nav');
+
+$header_top.find('a').on('click', function() {
+  $(this).parent().toggleClass('open-menu');
+});
+
+$('#fullpage').fullpage({
+  sectionsColor: ['#3dcfa1', '#fc4e5a', '#fffff', '#feaa01', '#fc4e5a'],
+  sectionSelector: '.vertical-scrolling',
+  navigation: true,
+  slidesNavigation: true,
+  controlArrows: false,
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+  menu: '#menu',
+  normalScrollElements: '.term',
+  afterLoad: function(anchorLink, index) {
+    $header_top.css('background', 'rgba(0, 47, 77, .3)');
+    $nav.css('background', 'rgba(0, 47, 77, .25)');
+    // if (index == 5) {
+    //     $('#fp-nav').hide();
+    //   }
+  },
+
+  onLeave: function(index, nextIndex, direction) {
+    if(index == 5) {
+      $('#fp-nav').show();
+    }
+  },
+
+
+});
